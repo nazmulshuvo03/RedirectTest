156cc21f5f7050567a56bb95b25b7723829a9960
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-11-01 12:28:52 +0600

    redirect to a tag

--- dummy.php
+++ dummy.php
@@ -2,7 +2,7 @@
     $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
     if (strpos($userAgent, 'Instagram')) {
-        header('Content-type: application/pdf');
+        header('Content-type: application/json');
         header('Content-Disposition: inline; filename= blablabla');
         header('Content-Transfer-Encoding: binary');
         header('Accept-Ranges: bytes');
